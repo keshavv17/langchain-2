6a35e319ee93ada1f8a260dddbb769a9ed73c4e7
keshavv17 <keshavsharma518@gmail.com>
2025-09-13 09:45:59 +0530

    tools-agents

--- 10-tools-agents/app.py
+++ 10-tools-agents/app.py
@@ -37,4 +37,19 @@ if 'messages' not in st.session_state:
     ]
     
 for msg in st.session_state.messages:
-    st.chat_message(msg['role'].write(msg['content']))
+    st.chat_message(msg['role']).write(msg['content'])
+    
+if prompt:=st.chat_input(placeholder='what is machine learning'):
+    st.session_state.messages.append({'role':'user', 'content': prompt})
+    st.chat_message('user').write(prompt)
+    
+    llm = ChatGroq(groq_api_key = api_key, model_name = 'llama-3.1-8b-instant', streaming = True)
+    tools = [search, arxiv, wiki]
+    
+    search_agent = initialize_agent(tools, llm, agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION, handling_parsing_errors = True)
+    
+    with st.chat_message('assistant'):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({'role':'assistant', 'content':response})
+        st.write(response)
